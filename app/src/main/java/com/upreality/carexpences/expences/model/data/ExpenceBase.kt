b55767f925e9ddca0c1f69100c7274e4dc194082
Denis Rebrov <denisrebrof@gmail.com>
2020-12-06 17:02:10 +0300

    fixed primary key autogen

--- app/src/main/java/com/upreality/carexpences/expences/model/data/ExpenceBase.kt
+++ app/src/main/java/com/upreality/carexpences/expences/model/data/ExpenceBase.kt
@@ -4,7 +4,7 @@ import androidx.room.PrimaryKey
 import java.util.*
 
 open class ExpenceBase(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Long,
     val date: Date,
     val cost: Float
