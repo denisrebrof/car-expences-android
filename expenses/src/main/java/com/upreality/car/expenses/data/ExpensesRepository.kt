519742421e18c014caa4437570ff6cf6bee1d4a1
Denis Rebrov <denisrebrof@gmail.com>
2021-01-15 22:12:38 +0300

    fixed build error ( probably from incorrect project copy )

--- expenses/src/main/java/com/upreality/car/expenses/data/ExpensesRepository.kt
+++ expenses/src/main/java/com/upreality/car/expenses/data/ExpensesRepository.kt
@@ -4,25 +4,28 @@ import com.upreality.car.expenses.data.datasources.ExpensesLocalDataSource
 import com.upreality.car.expenses.domain.IExpensesRepository
 import com.upreality.car.expenses.domain.model.ExpenseFilter
 import com.upreality.car.expenses.domain.model.expence.Expense
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Maybe
 import javax.inject.Inject
 
 class ExpensesRepository @Inject constructor(
     private val expensesLocalDataSource: ExpensesLocalDataSource
 ) : IExpensesRepository {
 
-    override fun create(expense: Expense): Long {
+    override fun create(expense: Expense): Maybe<Long> {
         return expensesLocalDataSource.create(expense)
     }
 
-    override fun get(filter: ExpenseFilter): List<Expense> {
+    override fun get(filter: ExpenseFilter): Flowable<List<Expense>> {
         return expensesLocalDataSource.get(filter)
     }
 
-    override fun update(expense: Expense) {
-        expensesLocalDataSource.update(expense)
+    override fun update(expense: Expense) : Completable {
+        return expensesLocalDataSource.update(expense)
     }
 
-    override fun delete(expense: Expense) {
-        expensesLocalDataSource.delete(expense)
+    override fun delete(expense: Expense) : Completable {
+        return expensesLocalDataSource.delete(expense)
     }
 }
