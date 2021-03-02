9db0c3e73714514c4f7f7418dfd3743e6d748352
Denis Rebrov <denisrebrof@gmail.com>
2021-03-02 15:01:22 +0300

    refactor

---
+++ expenses/src/main/java/com/upreality/car/expenses/data/local/expenses/model/queries/ExpensePaginatedFilter.kt
@@ -0,0 +1,14 @@
+package com.upreality.car.expenses.data.local.expenses.model.queries
+
+import com.upreality.car.common.data.IDatabaseFilter
+
+abstract class ExpensePaginatedFilter(private val cursor: Long, private val length: Int) : IDatabaseFilter {
+
+    abstract val orderColumn: String
+    abstract val sortAscending: Boolean
+
+    override fun getFilterExpression(): String {
+        val compareOperator = if(sortAscending) '>' else '<'
+        return "SELECT * FROM expenses WHERE $orderColumn$compareOperator$cursor ORDER BY orderColumn LIMIT $length"
+    }
+}
