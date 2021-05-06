355edb86c3875df8916a72880329028d97214850
Denis Rebrov <denisrebrof@gmail.com>
2021-05-07 02:49:26 +0300

    fixed subscribeon

--- expenses/src/main/java/com/upreality/car/expenses/data/paging/ExpensesPagingSource.kt
+++ expenses/src/main/java/com/upreality/car/expenses/data/paging/ExpensesPagingSource.kt
@@ -6,6 +6,8 @@ import com.upreality.car.expenses.domain.IExpensesRepository
 import com.upreality.car.expenses.domain.model.ExpenseFilter
 import com.upreality.car.expenses.domain.model.expence.Expense
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class ExpensesPagingSource @Inject constructor(
@@ -17,7 +19,11 @@ class ExpensesPagingSource @Inject constructor(
         val nextPage = params.key ?: 1
         val response = ExpenseFilter.Paged(nextPage, params.loadSize).let(repository::get)
 
-        return response.firstElement().toSingle().map { expensesList ->
+        return response
+            .firstElement()
+            .toSingle()
+            .subscribeOn(Schedulers.io())
+            .map { expensesList ->
             LoadResult.Page(
                 data = expensesList,
                 prevKey = if (nextPage == 1) null else nextPage - 1,
