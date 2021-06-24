5b3e133d52bd4800e72b014ce161b2c1ca9c0ff5
Denis Rebrov <denisrebrof@gmail.com>
2021-06-24 22:03:44 +0300

    setting up

--- app/src/main/java/com/upreality/car/CarExpensesApplication.kt
+++ app/src/main/java/com/upreality/car/CarExpensesApplication.kt
@@ -2,6 +2,11 @@ package com.upreality.car
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.realm.mongodb.App
+import javax.inject.Inject
 
 @HiltAndroidApp
-class CarExpensesApplication : Application()
+class CarExpensesApplication : Application() {
+    @Inject
+    lateinit var realmApp: App
+}
--- expenses/src/main/java/com/upreality/car/expenses/data/repository/ExpensesRealmRepositoryImpl.kt
+++ expenses/src/main/java/com/upreality/car/expenses/data/repository/ExpensesRealmRepositoryImpl.kt
@@ -12,6 +12,7 @@ import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.realm.Realm
 import io.realm.RealmQuery
+import java.util.*
 import javax.inject.Inject
 
 class ExpensesRealmRepositoryImpl @Inject constructor() : IExpensesRepository {
@@ -19,27 +20,28 @@ class ExpensesRealmRepositoryImpl @Inject constructor() : IExpensesRepository {
     private val baseRealm: Realm = Realm.getDefaultInstance()
 
     override fun create(expense: Expense): Completable {
-        val realm = Realm.getDefaultInstance()
-        val dataModel = ExpenseRealmConverter.fromDomain(expense)
+        expense.id = UUID.randomUUID().hashCode().toLong()
         return Completable.fromAction {
+            val realm = Realm.getDefaultInstance()
+            realm.beginTransaction()
+            val dataModel = ExpenseRealmConverter.fromDomain(expense, realm)
             realm.copyToRealmOrUpdate(dataModel)
-        }.doOnDispose {
+            realm.commitTransaction()
             realm.close()
         }
     }
 
     override fun get(filter: ExpenseFilter): Flowable<List<Expense>> {
         val realm = Realm.getDefaultInstance()
-        val expensesQuery = baseRealm.where(ExpenseRealm::class.java)
+        val expensesQuery = realm.where(ExpenseRealm::class.java)
         return when (filter) {
             ExpenseFilter.All -> expensesQuery
             ExpenseFilter.Fines -> filterQueryByType(expensesQuery, ExpenseType.Fines)
             ExpenseFilter.Fuel -> filterQueryByType(expensesQuery, ExpenseType.Fuel)
             ExpenseFilter.Maintenance -> filterQueryByType(expensesQuery, ExpenseType.Maintenance)
-            is ExpenseFilter.Paged -> expensesQuery.greaterThanOrEqualTo(
-                ExpenseRealmFields.ID,
-                filter.cursor
-            ).limit(filter.pageSize.toLong())
+            is ExpenseFilter.Paged -> expensesQuery
+//                .greaterThanOrEqualTo(ExpenseRealmFields.ID, filter.cursor)
+//                .limit(filter.pageSize.toLong())
         }.findAllAsync().asFlowable().map { results ->
             results.toList().map(ExpenseRealmConverter::toDomain)
         }
@@ -56,23 +58,25 @@ class ExpensesRealmRepositoryImpl @Inject constructor() : IExpensesRepository {
     }
 
     override fun update(expense: Expense): Completable {
-        val realm = Realm.getDefaultInstance()
-        val dataModel = ExpenseRealmConverter.fromDomain(expense)
         return Completable.fromAction {
+            val realm = Realm.getDefaultInstance()
+            realm.beginTransaction()
+            val dataModel = ExpenseRealmConverter.fromDomain(expense, realm)
             realm.copyToRealmOrUpdate(dataModel)
-        }.doOnDispose {
+            realm.commitTransaction()
             realm.close()
         }
     }
 
     override fun delete(expense: Expense): Completable {
-        val realm = Realm.getDefaultInstance()
         return Completable.fromAction {
+            val realm = Realm.getDefaultInstance()
+            realm.beginTransaction()
             realm.where(ExpenseRealm::class.java)
                 .contains(ExpenseRealmFields.ID, expense.id.toString())
                 .findFirst()
                 ?.deleteFromRealm()
-        }.doOnDispose {
+            realm.commitTransaction()
             realm.close()
         }
     }
