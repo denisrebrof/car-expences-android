ee9621d8d6db8b7b46c65821ccf35b12eec3ca06
Denis Rebrov <denisrebrof@gmail.com>
2021-07-06 20:23:56 +0300

    di fix

--- expenses/src/main/java/com/upreality/car/expenses/domain/di/ExpensesInteractorModule.kt
+++ expenses/src/main/java/com/upreality/car/expenses/domain/di/ExpensesInteractorModule.kt
@@ -6,9 +6,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ViewModelComponent
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ActivityComponent::class, ViewModelComponent::class)
 abstract class ExpensesInteractorModule {
     @Binds
     abstract fun provideInteractor(
