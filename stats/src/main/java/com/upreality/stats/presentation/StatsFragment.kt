8760e6e97a1624f19d220727f1f3051879c487cd
Denis Rebrov <denisrebrof@gmail.com>
2021-07-24 18:50:57 +0300

    setting up stats fragment

--- stats/src/main/java/com/upreality/stats/presentation/StatsFragment.kt
+++ stats/src/main/java/com/upreality/stats/presentation/StatsFragment.kt
@@ -1,32 +1,17 @@
 package com.upreality.stats.presentation
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.upreality.stats.R
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import com.upreality.stats.databinding.FragmentStatsBinding as ViewBinding
 
+@AndroidEntryPoint
 class StatsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = StatsFragment()
-    }
+    private val viewModel: StatsFragmentViewModel by viewModels()
+    private val binding: ViewBinding by viewBinding(ViewBinding::bind)
 
-    private lateinit var viewModel: StatsFragmentViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_stats, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StatsFragmentViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    
 
 }
--- stats/src/main/java/com/upreality/stats/presentation/StatsFragmentViewModel.kt
+++ stats/src/main/java/com/upreality/stats/presentation/StatsFragmentViewModel.kt
@@ -1,7 +1,9 @@
 package com.upreality.stats.presentation
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class StatsFragmentViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
