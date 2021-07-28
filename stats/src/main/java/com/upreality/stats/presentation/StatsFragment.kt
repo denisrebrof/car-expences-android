3da8b64beb293a4d65ac7cd1344b312d2b7897f2
Denis Rebrov <denisrebrof@gmail.com>
2021-07-29 00:24:50 +0300

    fixes

--- stats/src/main/java/com/upreality/stats/presentation/StatsFragment.kt
+++ stats/src/main/java/com/upreality/stats/presentation/StatsFragment.kt
@@ -14,7 +14,6 @@ import io.sellmair.disposer.disposers
 import presentation.RxLifecycleExtentions.subscribeDefault
 import com.upreality.stats.databinding.FragmentStatsMainBinding as ViewBinding
 
-
 @AndroidEntryPoint
 class StatsFragment : Fragment(R.layout.fragment_stats_main) {
 
@@ -35,9 +34,10 @@ class StatsFragment : Fragment(R.layout.fragment_stats_main) {
         val pieData = viewState.typesRelationMap.map { (type, value) -> PieEntry(value, type) }
         ExpenseTypeChartSetup.setup(pieChart, ArrayList(pieData))
 
+        val format: (Float) -> String = { value -> String.format("%.2f", value) }
         binding.statsTypesChartCard.apply {
-            viewState.ratePerLiter.toString().let(rpl::setValue)
-            viewState.ratePerMile.toString().let(rpm::setValue)
+            viewState.ratePerLiter.let(format).let(rpl::setValue)
+            viewState.ratePerMile.let(format).let(rpm::setValue)
         }
     }
 }
