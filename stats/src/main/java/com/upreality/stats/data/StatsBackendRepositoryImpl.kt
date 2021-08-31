8945d03656b197191228e10149f76b097571d6cd
Denis Rebrov <denisrebrof@gmail.com>
2021-08-31 15:31:19 +0300

    fixed stats api

--- stats/src/main/java/com/upreality/stats/data/StatsBackendRepositoryImpl.kt
+++ stats/src/main/java/com/upreality/stats/data/StatsBackendRepositoryImpl.kt
@@ -1,30 +1,40 @@
 package com.upreality.stats.data
 
+import android.util.Log
 import com.upreality.car.expenses.data.shared.converters.ExpenseTypeConverter
 import com.upreality.car.expenses.domain.ExpenseToTypeConverter
 import com.upreality.car.expenses.domain.model.ExpenseFilter
 import com.upreality.stats.domain.IStatsRepository
+import io.reactivex.Flowable
 import javax.inject.Inject
 
 class StatsBackendRepositoryImpl @Inject constructor(
     private val api: StatsBackendApi
 ) : IStatsRepository {
-    override fun getRatePerMile(filters: List<ExpenseFilter>) =
-        getRequest(filters).let(api::getRatePerMile)
 
-    override fun getRatePerLiter(filters: List<ExpenseFilter>) =
-        getRequest(filters).let(api::getRatePerLiter)
+    override fun getRatePerMile(filters: List<ExpenseFilter>): Flowable<Float> =
+        getRequest(filters).let(api::getRatePerMile).doOnNext {
+            Log.d("", "")
+        }
+
+    override fun getRatePerLiter(filters: List<ExpenseFilter>): Flowable<Float> =
+        getRequest(filters).let(api::getRatePerLiter).doOnNext {
+            Log.d("", "")
+        }
 
     override fun getTypesRateMap(filters: List<ExpenseFilter>) = getRequest(filters)
         .let(api::getTypesRateMap)
         .map(StatsTypeRateResponse::map)
         .map { map ->
-            map.mapKeys { (key, _) ->
-                ExpenseTypeConverter.fromId(key.toInt())
-            }
+            map.associateBy { it._id }
+                .mapKeys{ it.key.let(ExpenseTypeConverter::fromId) }
+                .mapValues { it.value.count }
         }
 
-    override fun getRate(filters: List<ExpenseFilter>) = getRequest(filters).let(api::getRate)
+    override fun getRate(filters: List<ExpenseFilter>): Flowable<Float> =
+        getRequest(filters).let(api::getRate).doOnNext {
+            Log.d("", "")
+        }
 
     private fun getRequest(filters: List<ExpenseFilter>) = StatsBackendRequest(
         getTypeIds(filters),
--- stats/src/main/java/com/upreality/stats/data/StatsTypeRateResponse.kt
+++ stats/src/main/java/com/upreality/stats/data/StatsTypeRateResponse.kt
@@ -3,5 +3,10 @@ package com.upreality.stats.data
 import com.upreality.car.expenses.data.shared.model.ExpenseType
 
 data class StatsTypeRateResponse(
-    val map: Map<Long, Float>? = null
+    val map: List<StatsTypeRateResponseMapEntry>? = null
+)
+
+data class StatsTypeRateResponseMapEntry(
+    val _id: Int,
+    val count: Float
 )
